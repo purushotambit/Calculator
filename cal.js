857d3f25dcf60ac1aa29b0ea3be48f6863586f35
Purushotam <purushpawan@gmail.com>
2022-12-07 22:42:24 +0530

    version 2

--- cal.js
+++ cal.js
@@ -1,24 +1,28 @@
-let string="";
-let buttons=document.querySelectorAll(".btn");
-console.log(buttons.size);
-let  array=Array.from(buttons);
-console.log(array.length);
-for(let i=0;i<array.length;i++){
-let button=array[i];
- button.addEventListener("click",(e)=>{
-    if(e.target.innerHTML=='='){
-    string=eval(string);
-    document.getElementById('input').value=string;
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+     string="";
     }
-    else if(e.target.innerHTML=='C'){
-        string="";
-        document.getElementById('input').value="";
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
     }
-    else{
-        string=string+e.target.innerHTML;
-        document.getElementById('input').value=string;
+    else if(e.target.innerHTML=='M+'){
+        if(string=="")
+        string="1000";
+        else{
+            string=string+"+1000";
+        }
+        document.querySelector('input').value = string;
     }
-   
- })
-
-}
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
